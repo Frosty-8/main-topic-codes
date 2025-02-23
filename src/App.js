1bed829a13ed410d6d1dcca2318ced323ab41051
Sarthak <sarthakdongare611@gmail.com>
2025-02-23 17:57:23 +0530

    Added SEO fixing to deploy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import { Mainfile } from "./components";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <>
-      <Mainfile/>
+      <Helmet>
+        <title>Python DL, ML & DSA Code Hub | Explore & Learn</title>
+        <meta name="description" content="Explore Python implementations of Deep Learning (DL), Machine Learning (ML), and Data Structures & Algorithms (DSA). Perfect for learners and developers." />
+        <meta name="keywords" content="Python, Deep Learning, Machine Learning, Data Structures, Algorithms, AI, Neural Networks, Code Implementations" />
+        <meta name="robots" content="index, follow" />
+      </Helmet>
+      <Mainfile />
     </>
   );
 }
